715a38e73f6b010ad5191c786d4cad0da3f0f511
Floris <sk8_floris@hotmail.com>
2016-04-11 11:47:23 +0200

    started, phonebook is parsed

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 
 
+
+FILENAME = "phonebook.txt"
+COMMANDS = ["call", "offhook", "onhook", "transfer", "conference"]
+
+phonenumbers = {}
+
+def parse_phonebook():
+
+
+    with open(FILENAME) as f:
+        for line in f:
+            number, name = line.split(' ', 1)
+            number = number.strip()
+            name = name.strip()
+            if number.isdigit() and (len(number) == 5) and name.isalpha() and (len(name) <= 12):
+                phonenumbers[number] = name
+
+
+
 def main():
+    
     print "Hello World!"
+    
+    parse_phonebook()
+
+    print "Phonenumber dictionary:"
+    print phonenumbers
+
+
+
     return 0
 
 if __name__ == '__main__':
