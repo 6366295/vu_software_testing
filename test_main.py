7213fd437eb29da09d45a9c600341f1bcac770d8
Baris Can Vural <bcanvural@sabanciuniv.edu>
2016-04-25 14:28:39 +0200

    Add two test cases, example of catching stdout

--- test_main.py
+++ test_main.py
@@ -4,6 +4,7 @@ from commands import *
 from phone import Phonebook 
 from commands import UserCommands
 import sys
+from StringIO import StringIO
 
 class MyTest(unittest.TestCase):
     def setUp(self):
@@ -11,15 +12,33 @@ class MyTest(unittest.TestCase):
         self.phonebook = Phonebook()
         self.phonebook.parse_phonebook("phonebook.txt")
         self.user_commands = UserCommands(self.phonebook)
+        self.saved_out = sys.stdout
+        self.out = StringIO()
+        sys.stdout = self.out
         
     def tearDown(self):
         #cleanup after tests
-        pass
-    
+        sys.stdout = self.saved_out 
    #Testing cmd_call
    #b1-b2-b4
     def test(self):
-        self.user_commands.cmd_call(phone1="foooooo", phone2="bar")
+        phone1 = "foooo"
+        self.user_commands.cmd_call(phone1=phone1, phone2="bar")
+        self.assertEqual(phone1 +  " does not exist!", self.out.getvalue().strip())
+    
+    #Testing cmd_call 
+    #b1-b2-b3-b5-b10
+    def test2(self):
+        phone1 = "foo"
+        self.user_commands.cmd_call(phone1=phone1, phone2="bar")
+        self.assertEqual(phone1 +  " hears silence", self.out.getvalue().strip())
+    
+    #Testing cmd_call
+    #b1-b2-b3-b5-b6-b7-b10
+    def test3(self):
+        pass
+        
+        
         
            
             
