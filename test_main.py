2e406cabc51eb05921fb2c078904b74a2ea8969e
Baris Can Vural <bcanvural@sabanciuniv.edu>
2016-04-25 14:06:03 +0200

    Add more to testing file

--- test_main.py
+++ test_main.py
@@ -1,20 +1,27 @@
+# python -m unittest test_main
 import unittest
-from phone import Phonebook
+from commands import *
+from phone import Phonebook 
+from commands import UserCommands
+import sys
 
 class MyTest(unittest.TestCase):
     def setUp(self):
         #setup before tests
+        self.phonebook = Phonebook()
+        self.phonebook.parse_phonebook("phonebook.txt")
+        self.user_commands = UserCommands(self.phonebook)
         
     def tearDown(self):
         #cleanup after tests
+        pass
     
+   #Testing cmd_call
+   #b1-b2-b4
     def test(self):
-           phonebook = Phonebook.__new__(Phonebook)
-           phonebook.parse_phonebook("phonebok.txt")
+        self.user_commands.cmd_call(phone1="foooooo", phone2="bar")
+        
            
-    def test2(self):
-           phonebook = Phonebook.__new__(Phonebook)
-           phonebook.parse_phonebook("phonebo.txt")
             
            
         
